89622cf1ada61b48378962e574a536e2104b70f7
Sam Oehlert <sam.oehlert@gmail.com>
2019-06-15 19:38:28 -0500

    style(imports): change imports to be on separate lines

--- pyflare.py
+++ pyflare.py
@@ -1,12 +1,17 @@
 #! /usr/bin/python
-import json, os
-
+import json
+import os
 import requests
+
 
 class Cloudflare:
     def __init__(self, email, key):
         self.endpoint = "https://api.cloudflare.com/client/v4"
-        self.headers = {'X-Auth-Email': email, 'X-Auth-Key': key, 'Content-Type': 'application/json'}
+        self.headers = {
+            "X-Auth-Email": email,
+            "X-Auth-Key": key,
+            "Content-Type": "application/json",
+        }
 
     def getmyip(self):
         r = requests.get("https://api.ipify.org/")
@@ -17,39 +22,51 @@ class Cloudflare:
         return r.json()
 
     def zones(self, zone):
-        payload = {'name': zone}
+        payload = {"name": zone}
         r = requests.get(self.endpoint + "/zones", headers=self.headers, params=payload)
         return r.json()
 
     def dns_records(self, zone_id, record):
-        payload = {'name': record}
-        r = requests.get(self.endpoint + "/zones/" + zone_id + "/dns_records", headers=self.headers, params=payload)
+        payload = {"name": record}
+        r = requests.get(
+            self.endpoint + "/zones/" + zone_id + "/dns_records",
+            headers=self.headers,
+            params=payload,
+        )
         return r.json()
 
     def update_record(self, zone_id, record_id, record, ip_address):
-        payload = {'type': 'A', 'name': record, 'content': ip_address}
-        r = requests.put(self.endpoint + "/zones/" + zone_id + "/dns_records/" + record_id, headers=self.headers, data=json.dumps(payload))
+        payload = {"type": "A", "name": record, "content": ip_address}
+        r = requests.put(
+            self.endpoint + "/zones/" + zone_id + "/dns_records/" + record_id,
+            headers=self.headers,
+            data=json.dumps(payload),
+        )
         return r.json()
 
-    def __call__(self,zone,record):
-        zone_id = cf.zones(zone)['result'][0]['id']
-        record_id = cf.dns_records(zone_id, record)['result'][0]['id']
+    def __call__(self, zone, record):
+        zone_id = cf.zones(zone)["result"][0]["id"]
+        record_id = cf.dns_records(zone_id, record)["result"][0]["id"]
         ip_address = cf.getmyip()
-        if ip_address != cf.dns_records(zone_id, record)['result'][0]['content']:
+        if ip_address != cf.dns_records(zone_id, record)["result"][0]["content"]:
             return cf.update_record(zone_id, record_id, record, ip_address)
         else:
             return "OK"
 
-if __name__ == '__main__':
-	__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-	try:
-		with open(os.path.join(__location__,'config.json')) as json_data_file:
-			config = json.load(json_data_file)
-			email = config['email']
-			key = config['key']
-			zone = config['zone']
-			record = config['record']
-		cf = Cloudflare(email, key)
-		print(cf(zone,record))
-	except IOError:
-		print("Unable to find config file.")
+
+if __name__ == "__main__":
+    __location__ = os.path.realpath(
+        os.path.join(os.getcwd(), os.path.dirname(__file__))
+    )
+    try:
+        print("opening config file")
+        with open(os.path.join(__location__, "config.json")) as json_data_file:
+            config = json.load(json_data_file)
+            email = config["email"]
+            key = config["key"]
+            zone = config["zone"]
+            record = config["record"]
+        cf = Cloudflare(email, key)
+        print(cf(zone, record))
+    except IOError:
+        print("Unable to find config file.")
